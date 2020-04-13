b195290c4859e2d80516abe8973b7cbce1247e73
Fabsl22 <fabian@fabians-mbp.home>
2020-04-13 14:49:00 +0200

    fixed Test Dataset Error

--- assignment1/linear_cats_dogs.py
+++ assignment1/linear_cats_dogs.py
@@ -14,7 +14,7 @@ TrainedModel = namedtuple('TrainedModel', ['model', 'accuracy'])
 
 train_data = PetsDataset("cifar-10-batches-py/", Subset.TRAINING)
 val_data = PetsDataset("cifar-10-batches-py/", Subset.VALIDATION)
-test_data = PetsDataset('cifar-10-batches-py/', Subset.VALIDATION)
+test_data = PetsDataset('cifar-10-batches-py/', Subset.TEST)
 
 
 op = ops.chain([
@@ -51,39 +51,49 @@ def train_model(lr: float, momentum: float) -> TrainedModel:
     return TrainedModel(clf, accuracy)
 
 
-# grid search
-## options
-lr_options = [0.1, 0.001, 0.0001]
-momentum_options = [0.01, 0.1, 0.001]
-models = []
-best_model = TrainedModel(None, Accuracy())
-
-
-
-# loop over all combinations
-for lr in lr_options:
-    for momentum in momentum_options:
-        nesterov = True
-        print('Training with parameters: lr={}, momentum={}, nesterov={}'.format(lr, momentum, nesterov), end='\r')
-        model = train_model(lr, momentum)
-        models.append(model)
-
-for model in models:
-    if model.accuracy.__gt__(best_model.accuracy):
-        best_model = model
+def grid_search(lr_options, momentum_options, nesterov):
+    """Make a Grid Search with the given options
     
-test_accuracy = Accuracy()
-for batch in test_batches:
-    prediction = best_model.model.predict(batch.data)
-    test_accuracy.update(prediction, batch.label)
+    Return the best model(TrainedModel) and a Pandas DataFrae with all validation accuracies
+    """
 
-print('Best Model:')
-print('Parameters: lr={}, momentum={}'.format(best_model.model.lr, best_model.model.momentum))
-print('Test Accuracy = {}'.format(test_accuracy.accuracy()))
+    models = []
+    best_model = TrainedModel(None, Accuracy())
 
-# create a dataframe with all validation accuracies 
-result_df = pd.DataFrame(index=lr_options, columns=momentum_options)
-for model in models:
-    result_df.at[model.model.lr, model.model.momentum] = model.accuracy.accuracy()
+
+
+    # loop over all combinations
+    for lr in lr_options:
+        for momentum in momentum_options:
+            nesterov = True
+            print('Training with parameters: lr={}, momentum={}, nesterov={}  '.format(lr, momentum, nesterov), end='\r')
+            model = train_model(lr, momentum)
+            models.append(model)
+            if model.accuracy.__gt__(best_model.accuracy):
+                best_model = model
+
+        
+    test_accuracy = Accuracy()
+    for batch in test_batches:
+        prediction = best_model.model.predict(batch.data)
+        test_accuracy.update(prediction, batch.label)
+
+    print('\nBest Model:')
+    print('Parameters: lr={}, momentum={}'.format(best_model.model.lr, best_model.model.momentum))
+    print('Test Accuracy = {}'.format(test_accuracy.accuracy()))
+
+    # create a dataframe with all validation accuracies 
+    result_df = pd.DataFrame(index=lr_options, columns=momentum_options)
+    result_df.index.name = 'Learning Rate'
+    result_df.columns.name = 'Momentum'
+    for model in models:
+        result_df.at[model.model.lr, model.model.momentum] = model.accuracy.accuracy()
+
+    return (best_model, result_df)
+
+
+lr_options = [0.1, 0.01, 0.001]
+momentum_options = [0.1, 0.01, 0.001]
+best_model, result_df = grid_search(lr_options=lr_options, momentum_options=momentum_options, nesterov=True)
 
 print(result_df)
